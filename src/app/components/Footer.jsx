224fd519f271acf94f06137486a10e81e0085f1f
krishnakantnitp2025 <krishnav.ug21.ee@nitp.ac.in>
2024-06-24 21:00:33 +0530

    footer changed

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,9 +4,9 @@ const Footer = () => {
     return (
         <footer className="footer border-t-2 border-l-transparent border-r-transparent text-white">
             <div className="container p-6 flex justify-between">
-                <span>KK</span>
+                <span>Made with ❤️ in India</span>
                 <p className="text-slate-600">
-                    All Rights Reserved &copy; Krishna
+                    &copy; 2024 Krishna Kant Verma
                 </p>
             </div>
         </footer>
